f9bb969905bf9b11cdb4d048144035f81cee31b1
Evgeny <afazhenya2001@gmail.com>
2022-11-20 00:46:47 +0300

    fileStorage gateway

--- fileStorageGatewayService/src/index.ts
+++ fileStorageGatewayService/src/index.ts
@@ -1,9 +1,10 @@
 import express, {Express, Request, Response} from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import s3 from './s3StorageApi/index.js'
 
 if (process.env.NODE_ENV !== 'production'){
-    dotenv.config();
+    dotenv.config()
 }
 
 if (!process.env.PORT) {
@@ -14,12 +15,31 @@ const app: Express = express();
 app.use(cors())
 app.use(express.json())
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.get('/', (req:Request, res:Response)=>{
-    res.send('hello from file service' +
-        '')
+    res.send('hello from file service')
 });
+
+/**
+ * TODO:
+ *  - Leave the dump logic, specific for images transfer
+ *  - Extend get req params and API
+ *  - API tests
+ * */
+app.get('/file', async (req:Request, res:Response)=>{
+    try{
+        const path = req.query.path
+        const fileData = await s3.Download(path)
+        const file = fileData.data
+        const b64Data = Buffer.from(file.Body).toString('base64');
+        res.status(200).send(b64Data)
+    }
+    catch (e) {
+        res.status(500).json(e)
+    }
+})
+
 
 app.listen(PORT,()=> {
     console.log('Server is running in port:' , PORT)
---
+++ fileStorageGatewayService/src/s3StorageApi/index.ts
@@ -0,0 +1,27 @@
+// @ts-ignore Library does not have any declaration files and @types/ lib
+import EasyYandexS3 from 'easy-yandex-s3';
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config()
+}
+
+if (!process.env.YSTORAGE_ACCESS_KEY_ID
+    || !process.env.YSTORAGE_SECRET_ACCESS_KEY_ID) {
+    throw new Error('Please, provide access secrets for Yandex Cloud storage')
+}
+
+/**
+ * Gateway to proceed CRUD operations with yandex storage
+ * Todo: check aws s3 api and create own version
+ * */
+const s3 = new EasyYandexS3({
+    auth: {
+        accessKeyId: process.env.YSTORAGE_ACCESS_KEY_ID,
+        secretAccessKey: process.env.YSTORAGE_SECRET_ACCESS_KEY_ID
+    },
+    Bucket: 'rtt-storage',
+    debug: false
+})
+
+export default s3

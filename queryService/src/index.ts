e5abc2023eacb071fa3a98c05eeae9230291f8ea
Evgeny Afanasyev <evgeny@MacBook-Air-Evgeny.local>
2023-01-04 17:49:30 +0300

    add init query service

---
+++ queryService/src/index.ts
@@ -0,0 +1,26 @@
+import express, {Express, Request, Response} from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+if (process.env.NODE_ENV !== 'production'){
+    dotenv.config();
+}
+
+if (!process.env.PORT) {
+    throw new Error('Please, specify port number!')
+}
+
+const app: Express = express();
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT;
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send('hello from query service' +
+        '')
+});
+
+app.listen(PORT,()=> {
+    console.log('Server is running in port:' , PORT)
+});
